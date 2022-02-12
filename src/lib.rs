edf93aa31d6f70ce2605311b659295bb8f2c039d
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-02-11 21:00:57 -0700

    Partially reimplemented basic parsing for ConstTraitImpl. Have not added support for covariant constant trait bounds

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
 use proc_macro::TokenStream;
-use proc_macro2::{Delimiter as Delimiter2, Group as Group2, TokenStream as TokenStream2};
+use proc_macro2::{Delimiter as Delimiter2, Group as Group2, TokenStream as TokenStream2, Span as Span2};
 use quote::ToTokens;
 use syn::{
     parse_macro_input,
-    token::{Bang, Brace, Const, Default, For, Impl, Unsafe},
-    Attribute, Generics, ImplItem, ItemImpl, Path, Type,
+    token::{Bang, Brace, Const, Default, For, Impl, Unsafe, Lt, Gt, Comma},
+    Result, Attribute, ImplItem, Path, Type, parse::{Parse, ParseStream}, punctuated::Punctuated, GenericParam, WhereClause, Token, Ident, Lifetime, Generics, TypePath, Error, braced, AttrStyle, bracketed, spanned::Spanned,
 };
+
+// syn::Generics is outdated because syn::GenericParam is. 
+
+// struct Generics {
+//     lt_token: Option<Lt>,
+//     params: Punctuated<GenericParam, Comma>,
+//     gt_token: Option<Gt>,
+//     where_clause: Option<WhereClause>,
+// }
 
 struct ItemConstImpl {
     attrs: Vec<Attribute>,
@@ -21,31 +30,119 @@ struct ItemConstImpl {
     items: Vec<ImplItem>,
 }
 
-impl From<ItemImpl> for ItemConstImpl {
-    fn from(item_impl: ItemImpl) -> ItemConstImpl {
-        ItemConstImpl {
-            attrs: item_impl.attrs,
-            defaultness: item_impl.defaultness,
-            unsafety: item_impl.unsafety,
-            impl_token: item_impl.impl_token,
-            generics: item_impl.generics,
-            constness: Some(Const::default()),
-            trait_: item_impl.trait_,
-            self_ty: item_impl.self_ty,
-            brace_token: item_impl.brace_token,
-            items: item_impl.items,
+// syn::attr::parsing::parse_inner (syn 1.0.86)
+fn single_parse_inner(input: ParseStream) -> Result<Attribute> {
+    let content;
+    Ok(Attribute {
+        pound_token: input.parse()?,
+        style: AttrStyle::Inner(input.parse()?),
+        bracket_token: bracketed!(content in input),
+        path: content.call(Path::parse_mod_style)?,
+        tokens: content.parse()?,
+    })
+}
+
+// syn::attr::parsing::parse_inner (syn 1.0.86)
+fn parse_inner(input: ParseStream, attrs: &mut Vec<Attribute>) -> Result<()> {
+    while input.peek(Token![#]) && input.peek2(Token![!]) {
+        attrs.push(input.call(single_parse_inner)?);
+    }
+    Ok(())
+}
+
+impl Parse for ItemConstImpl {
+    // Largely based on: https://docs.rs/syn/1.0.86/src/syn/item.rs.html#2402-2407
+    fn parse(input: syn::parse::ParseStream) -> Result<Self> {
+        let mut attrs = input.call(Attribute::parse_outer)?;
+        let defaultness = input.parse::<Option<Default>>()?;
+        let unsafety = input.parse::<Option<Token![unsafe]>>()?;
+        let impl_token = input.parse::<Impl>()?;
+
+        let has_generics = input.peek(Token![<])
+            && (input.peek2(Token![>])
+                || input.peek2(Token![#])
+                || (input.peek2(Ident) || input.peek2(Lifetime))
+                    && (input.peek3(Token![:])
+                        || input.peek3(Token![,])
+                        || input.peek3(Token![>])
+                        || input.peek3(Token![=]))
+                || input.peek2(Token![const]));
+        let mut generics: Generics = if has_generics {
+            input.parse::<Generics>()?
+        } else {
+            Generics::default()
+        };
+
+        let is_const_impl =
+            input.peek(Token![const]);
+            // The author is uncertain where the second kind of const impl comes from
+            // || input.peek(Token![?]) && input.peek2(Token![const]);
+        let constness = if is_const_impl {
+            // input.parse::<Option<Token![?]>>()?;
+            Some(input.parse::<Token![const]>()?)
+        } else { None };
+        let polarity = if input.peek(Token![!]) && !input.peek2(Brace) {
+            Some(input.parse::<Token![!]>()?)
+        } else {
+            None
+        };
+        let first_ty_span = input.span();
+        let mut first_ty: Type = input.parse::<Type>()?;
+        let self_ty: Type;
+        let trait_;
+        
+        let is_impl_for = input.peek(Token![for]);
+        if is_impl_for {
+            let for_token: Token![for] = input.parse::<Token![for]>()?;
+            let mut first_ty_ref = &first_ty;
+            while let Type::Group(ty) = first_ty_ref {
+                first_ty_ref = &ty.elem;
+            }
+            if let Type::Path(TypePath { qself: None, .. }) = first_ty_ref {
+                while let Type::Group(ty) = first_ty {
+                    first_ty = *ty.elem;
+                }
+                if let Type::Path(TypePath { qself: None, path}) = first_ty {
+                    trait_ = Some((polarity, path, for_token));
+                } else {
+                    unreachable!();
+                }
+            }
+            else {
+                return Err(Error::new(first_ty_span, "expected trait path"));
+            }
+            self_ty = input.parse()?;
+        } else {
+            return Err(Error::new(Span2::call_site(), "expected trait impl block"));
+        };
+        generics.where_clause = input.parse()?;
+
+        let content;
+        let brace_token = braced!(content in input);
+        parse_inner(&content, &mut attrs)?;
+
+        let mut items = Vec::new();
+        while !content.is_empty() {
+            items.push(content.parse()?);
+        }
+        if is_impl_for && trait_.is_none() {
+            return Err(Error::new(is_impl_for.span(), "expected trait name"));
+        } else {
+            Ok(ItemConstImpl {
+                attrs,
+                defaultness,
+                unsafety,
+                impl_token,
+                generics,
+                constness,
+                trait_,
+                self_ty: Box::new(self_ty),
+                brace_token,
+                items,
+            })
         }
     }
 }
-
-// impl Parse for ItemConstImpl {
-//     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-//         let attrs = input.call(Attribute::parse_outer)?;
-//         let defaultness = input.parse::<Option<Default>>()?;
-//         let unsafety = input.parse::<Unsafe>()?;
-//         let impl_token = input.parse::<Impl>()?;
-//     }
-// }
 
 impl From<ItemConstImpl> for TokenStream {
     #[allow(unused_variables, clippy::let_and_return)]
@@ -91,15 +188,14 @@ impl From<ItemConstImpl> for TokenStream {
                 .to_token_stream()
                 .into(),
         );
-        let comment = format!("const S: &str = \"{}\";", ts);
-        let ts = <TokenStream as std::str::FromStr>::from_str(&comment).unwrap();
+        // let comment = format!("const S: &str = \"{}\";", ts);
+        // let ts = <TokenStream as std::str::FromStr>::from_str(&comment).unwrap();
         ts
     }
 }
 
 #[proc_macro_attribute]
 pub fn const_trait_impl(_attr_args: TokenStream, item: TokenStream) -> TokenStream {
-    let item_impl: ItemImpl = parse_macro_input!(item as ItemImpl);
-    let item_const_impl: ItemConstImpl = item_impl.into();
+    let item_const_impl = parse_macro_input!(item as ItemConstImpl);
     item_const_impl.into()
 }
